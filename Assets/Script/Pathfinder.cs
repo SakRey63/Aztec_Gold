212adb1dcaf6863cd34363449c338890f195e3fe
SakRey63 <Sakrey63@gmail.com>
2024-02-29 21:16:28 +0400

    Double Jump for Pathfinder

--- Assets/Script/Pathfinder.cs
+++ Assets/Script/Pathfinder.cs
@@ -14,17 +14,47 @@ public class Pathfinder : MonoBehaviour
     private MovingToTheRight _movingToTheRight;
     private MovingToTheLeft _movingToTheLeft;
     private EnemySpawner _spawnFishes;
+    private Rigidbody _rb;
 
+    private bool _isGraunded;
     private bool _dobleJump;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
         transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * _movementSpeed);
         transform.Rotate(Vector3.up * _rotateSpeed * Input.GetAxis("Horizontal"));
-        if (Input.GetKey(KeyCode.Space))
-        {
-            transform.Translate((Vector3.up * _jumpPower * Time.deltaTime));
-        }
         
+        if (_isGraunded && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * _jumpPower);
+        }
+        if (_dobleJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * _jumpPower);
+            _dobleJump = false;
+        }
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            _isGraunded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            
+            _isGraunded = false;
+            _dobleJump = true;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
